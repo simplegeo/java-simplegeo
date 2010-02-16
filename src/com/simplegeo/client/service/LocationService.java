9222d452fdc9349ad0800a64db1effcedadb9596
dsmith <derek@simplegeo.com>
2010-02-15 17:55:10 -0700

    More documentation

--- src/com/simplegeo/client/service/LocationService.java
+++ src/com/simplegeo/client/service/LocationService.java
@@ -43,8 +43,9 @@ import com.simplegeo.client.model.IRecord;
 import com.simplegeo.client.utilities.SimpleGeoUtilities;
 
 /**
+ * A service
+ * 
  * @author Derek Smith
- *
  */
 public class LocationService {
 	
@@ -62,16 +63,18 @@ public class LocationService {
 	private GeoJSONHandler geoJSONHandler = null;
 	private SimpleGeoHandler simpleGeoHandler = null;
 	
+	/**
+	 * Tells the service whether to make the Http call on the same thread.
+	 */
 	public boolean futureTask = false; 
 	
 	/**
-	 * @author dsmith
-	 *
+	 * Enums that are used to differentiate between different handlers.
 	 */
 	static public enum Handler { GEOJSON, RECORD, SIMPLEGEO }
 	
 	/**
-	 * @return
+	 * @return the shared instance of this class
 	 */
 	static public LocationService getInstance() {
 		
@@ -93,8 +96,15 @@ public class LocationService {
 	}
 
 	/**
-	 * @param type
-	 * @param handler
+	 * Set the handler per type.
+	 * 
+	 * This is useful if subclasses or implementations of the model
+	 * objects change and the response object needs to be built 
+	 * differently.
+	 * 
+	 * @param type the Handler type to override
+	 * @param handler the handler must be a subclass of SimpleGeoHandler to
+	 * ensure that a valid response was recieved
 	 */
 	public void setHandler(Handler type, SimpleGeoHandler handler) {
 		
@@ -111,8 +121,10 @@ public class LocationService {
 	}
 	
 	/**
-	 * @param type
-	 * @return
+	 * @param type the type of handler to retrieved defined by 
+	 * {@link com.simplegeo.client.service.Handler}
+	 * @return the instance of {@link com.simplegeo.client.http.SimpleGeoHandler}
+	 * that is associated with the type
 	 */
 	public SimpleGeoHandler getHandler(Handler type) {
 		
@@ -134,8 +146,15 @@ public class LocationService {
 	}
 		
 	/**
+	 * Retrieves information from SimpleGeo about the 
+	 * {@link com.simplegeo.client.model.IRecord}. The record
+	 * must already exist in SimpleGeo in order for the request to be
+	 * successful. 
+	 * 
 	 * @param defaultRecord
-	 * @return
+	 * @return if {@link com.simplegeo.client.service.LocationService#futureTask} is false
+	 * then the return value will be the result of the response based on the handler used. Otherwise,
+	 * the return value will be a {@link java.util.concurrent.FutureTask}.
 	 * @throws ClientProtocolException
 	 * @throws IOException
 	 */
@@ -150,10 +169,18 @@ public class LocationService {
 	}	
 					
 	/**
+	 * Retrieves information from SimpleGeo about all of the 
+	 * {@link com.simplegeo.client.model.IRecord}s within the list. Atleast
+	 * one record must already exist in SimpleGeo in order for the request to
+	 * be successful.
+	 * 
 	 * @param records
-	 * @return
+	 * @return if {@link com.simplegeo.client.service.LocationService#futureTask} is false
+	 * then the return value will be the result of the response based on the handler used. Otherwise,
+	 * the return value will be a {@link java.util.concurrent.FutureTask}.
 	 * @throws ClientProtocolException
 	 * @throws IOException
+	 * @see <a href="http://help.simplegeo.com/faqs/api-documentation/endpoints"</a>
 	 */
 	public Object retrieve(List<IRecord> records) throws ClientProtocolException, IOException {
 		
@@ -167,12 +194,18 @@ public class LocationService {
 	}
 	
 	/**
-	 * @param layer
-	 * @param recordIds
-	 * @param handler
-	 * @return
+	 * Retrieve a information from SimpleGeo about all the record ids within
+	 * the list that are contained within a layer.
+	 * 
+	 * @param layer the name of the layer to find the records in
+	 * @param recordIds the ids of records to retrieve
+	 * @param handler the handler responsible for creating the return object
+	 * @return if {@link com.simplegeo.client.service.LocationService#futureTask} is false
+	 * then the return value will be the result of the response based on the handler used. Otherwise,
+	 * the return value will be a {@link java.util.concurrent.FutureTask}.
 	 * @throws ClientProtocolException
 	 * @throws IOException
+	 * @see <a href="http://help.simplegeo.com/faqs/api-documentation/endpoints"</a>
 	 */
 	private Object retrieve(String layer, String recordIds, SimpleGeoHandler handler) throws ClientProtocolException, IOException {
 		
@@ -184,42 +217,60 @@ public class LocationService {
 	}
 		
 	/**
-	 * @param record
-	 * @return
+	 * Updates the {@link com.simplegeo.client.model.IRecord} in SimpleGeo, creating the
+	 * record if necessary.
+	 * 
+	 * @param record the record to update
+	 * @return if {@link com.simplegeo.client.service.LocationService#futureTask} is false
+	 * then the return value will be the result of the response based on the handler used. Otherwise,
+	 * the return value will be a {@link java.util.concurrent.FutureTask}.
 	 * @throws ClientProtocolException
 	 * @throws IOException
+	 * @see <a href="http://help.simplegeo.com/faqs/api-documentation/endpoints"</a>
 	 */
 	public Object update(IRecord record) throws ClientProtocolException, IOException {		
 		return update(getGeoJSONObject(record));
 	}
 	
 	/**
-	 * @param records
-	 * @return
+	 * Updates the list of {@link com.simplegeo.client.model.IRecord} in SimpleGeo, creating
+	 * them if ncessary.
+	 * 
+	 * @param records the records to update
+	 * @return if {@link com.simplegeo.client.service.LocationService#futureTask} is false
+	 * then the return value will be the result of the response based on the handler used. Otherwise,
+	 * the return value will be a {@link java.util.concurrent.FutureTask}.
 	 * @throws ClientProtocolException
 	 * @throws IOException
+	 * @see <a href="http://help.simplegeo.com/faqs/api-documentation/endpoints"</a>
 	 */
 	public Object update(List<IRecord> records) throws ClientProtocolException, IOException {		
 		return update(getGeoJSONObject(records));
 	}
 
 	/**
-	 * @param geoJSONRecord
-	 * @return
+	 * Uses the {@link com.simplegeo.client.model.GeoJSONObject} to update records 
+	 * in SimpleGeo, creating the record if necessary.
+	 * 
+	 * @param geoJSONObject the geojson object to send in the request
+	 * @return if {@link com.simplegeo.client.service.LocationService#futureTask} is false
+	 * then the return value will be the result of the response based on the handler used. Otherwise,
+	 * the return value will be a {@link java.util.concurrent.FutureTask}.
 	 * @throws ClientProtocolException
 	 * @throws IOException
+	 * @see <a href="http://help.simplegeo.com/faqs/api-documentation/endpoints"</a>
 	 */
-	public Object update(GeoJSONObject geoJSONRecord) throws ClientProtocolException, IOException {
+	public Object update(GeoJSONObject geoJSONObject) throws ClientProtocolException, IOException {
 		
 		GeoJSONObject record = null;
-		if(geoJSONRecord.isFeature()) {
+		if(geoJSONObject.isFeature()) {
 			
 			record = new GeoJSONRecord("FeatureCollection");
 			
 			try {
 				
 				JSONArray features = new JSONArray();
-				features.put(geoJSONRecord);
+				features.put(geoJSONObject);
 				record.setFeatures(features);
 				
 			} catch (JSONException e) {
@@ -228,7 +279,7 @@ public class LocationService {
 			
 		} else {
 			
-			record = geoJSONRecord;
+			record = geoJSONObject;
 			
 		}
 		
@@ -237,12 +288,18 @@ public class LocationService {
 	}
 	
 	/**
-	 * @param layer
-	 * @param geoJSONObject
-	 * @param handler
-	 * @return
+	 * Uses the {@link com.simplegeo.client.model.GeoJSONObject} and layer name
+	 * to update records in SimpleGeo, creating the record if necessary.
+	 * 
+	 * @param layer the layer to add the records to
+	 * @param geoJSONObject the GeoJSON object that contains a FeatureCollection
+	 * @param handler the handler responsible for creating the return object
+	 * @return if {@link com.simplegeo.client.service.LocationService#futureTask} is false
+	 * then the return value will be the result of the response based on the handler used. Otherwise,
+	 * the return value will be a {@link java.util.concurrent.FutureTask}.
 	 * @throws ClientProtocolException
 	 * @throws IOException
+	 * @see <a href="http://help.simplegeo.com/faqs/api-documentation/endpoints"</a>
 	 */
 	public Object update(String layer, GeoJSONObject geoJSONObject, SimpleGeoHandler handler) 
 	throws ClientProtocolException, IOException {
@@ -259,10 +316,16 @@ public class LocationService {
 	}
 	
 	/**
-	 * @param record
-	 * @return
+	 * Deletes the {@link com.simplegeo.client.service.model.IRecord} from SimpleGeo.
+	 * The record must already exist in SimpleGeo in order for the request to be successful. 
+	 * 
+	 * @param record the record to delete
+	 * @return if {@link com.simplegeo.client.service.LocationService#futureTask} is false
+	 * then the return value will be the result of the response based on the handler used. Otherwise,
+	 * the return value will be a {@link java.util.concurrent.FutureTask}.
 	 * @throws ClientProtocolException
 	 * @throws IOException
+	 * @see <a href="http://help.simplegeo.com/faqs/api-documentation/endpoints"</a>
 	 */
 	public Object delete(IRecord record) throws ClientProtocolException, IOException {
 		
@@ -272,7 +335,21 @@ public class LocationService {
 		return null;
 	}
 	
-	private Object delete(String layer, String recordId, SimpleGeoHandler handler) 
+	/**
+	 * Deletes the record form SimpleGeo. The record must already exist in SimpleGeo in
+	 * order for the request to be successful.
+	 * 
+	 * @param layer
+	 * @param recordId
+	 * @param handler the handler responsible for creating the return object
+	 * @return if {@link com.simplegeo.client.service.LocationService#futureTask} is false
+	 * then the return value will be the result of the response based on the handler used. Otherwise,
+	 * the return value will be a {@link java.util.concurrent.FutureTask}.
+	 * @throws ClientProtocolException
+	 * @throws IOException
+	 * @see <a href="http://help.simplegeo.com/faqs/api-documentation/endpoints"</a>
+	 */
+	public Object delete(String layer, String recordId, SimpleGeoHandler handler) 
 		throws ClientProtocolException, IOException {
 		
 		String uri = mainURL + String.format("/records/%s/%s.json", layer, recordId);
@@ -281,13 +358,20 @@ public class LocationService {
 	}
 	
 	/**
-	 * @param geoHash
-	 * @param layers
-	 * @param types
+	 * Sends a nearby request to SimpleGeo using a geohash as the bounding box. If
+	 * layers is not specified, then all global layers will be used in the query. If
+	 * objects is not specfied, then all object types will be used in the query.
+	 * 
+	 * @param geoHash the area to search for records
+	 * @param layers the layers to search in
+	 * @param types the different types to look for
 	 * @param limit
-	 * @return
+	 * @return if {@link com.simplegeo.client.service.LocationService#futureTask} is false
+	 * then the return value will be the result of the response based on the handler used. Otherwise,
+	 * the return value will be a {@link java.util.concurrent.FutureTask}.
 	 * @throws ClientProtocolException
 	 * @throws IOException
+	 * @see <a href="http://help.simplegeo.com/faqs/api-documentation/endpoints"</a>
 	 */
 	public Object nearby(GeoHash geoHash, List<String> layers, List<String> types, int limit) 
 						throws ClientProtocolException, IOException {
@@ -296,14 +380,22 @@ public class LocationService {
 
 	
 	/**
-	 * @param geoHash
-	 * @param layers
-	 * @param types
-	 * @param limit
-	 * @param type
-	 * @return
+	 * Sends a nearby request to SimpleGeo using a geohash as the bounding box. This method
+	 * also provides a way to set the Handler used in the Http response. If
+	 * layers is not specified, then all global layers will be used in the query. If
+	 * objects is not specfied, then all object types will be used in the query. 
+	 * 
+	 * @param geoHash the area to search for records
+	 * @param layers the layers to search in
+	 * @param types the different types to look for
+	 * @param limit the maximum amount of records to return (defaults to 100)
+	 * @param type the handler responsible for creating the return object
+	 * @return if {@link com.simplegeo.client.service.LocationService#futureTask} is false
+	 * then the return value will be the result of the response based on the handler used. Otherwise,
+	 * the return value will be a {@link java.util.concurrent.FutureTask}.
 	 * @throws ClientProtocolException
 	 * @throws IOException
+	 * @see <a href="http://help.simplegeo.com/faqs/api-documentation/endpoints"</a>
 	 */
 	public Object nearby(GeoHash geoHash, List<String> layers, List<String> types, int limit, Handler type) 
 						throws ClientProtocolException, IOException {
@@ -321,31 +413,22 @@ public class LocationService {
 	}
 
 	/**
-	 * @param lat
-	 * @param lon
-	 * @param radius
-	 * @param layers
-	 * @param types
-	 * @param limit
-	 * @return
+	 * Sends a nearby request to SimpleGeo using the lat, lon and radius. If
+	 * layers is not specified, then all global layers will be used in the query. If
+	 * objects is not specfied, then all object types will be used in the query. 
+	 *
+	 * @param lat the latitude
+	 * @param lon the longitude
+	 * @param radius the radius of the search in km
+	 * @param layers the layers to search in
+	 * @param types the different types to look for
+	 * @param limit the maximum amount of records to return (defaults to 100)
+	 * @return if {@link com.simplegeo.client.service.LocationService#futureTask} is false
+	 * then the return value will be the result of the response based on the handler used. Otherwise,
+	 * the return value will be a {@link java.util.concurrent.FutureTask}.
 	 * @throws ClientProtocolException
 	 * @throws IOException
-	 */
-	public Object nearby(int lat, int lon, double radius, List<String> layers, List<String> types, int limit) 
-		throws ClientProtocolException, IOException {
-		return nearby(lat * 1000000.0, lon * 1000000.0, radius, layers, types, limit, Handler.GEOJSON);
-	}
-
-	/**
-	 * @param lat
-	 * @param lon
-	 * @param radius
-	 * @param layers
-	 * @param types
-	 * @param limit
-	 * @return
-	 * @throws ClientProtocolException
-	 * @throws IOException
+	 * @see <a href="http://help.simplegeo.com/faqs/api-documentation/endpoints"</a>
 	 */
 	public Object nearby(double lat, double lon, double radius, List<String> layers, List<String> types, int limit) 
 		throws ClientProtocolException, IOException {
@@ -354,16 +437,25 @@ public class LocationService {
 
 	
 	/**
-	 * @param lat
-	 * @param lon
-	 * @param radius
-	 * @param layers
-	 * @param types
-	 * @param limit
-	 * @param type
-	 * @return
+	 * Sends a nearby request to SimpleGeo using the lat, lon and radius. This method
+	 * also provides a way to set the Handler used in the Http response.  If
+	 * layers is not specified, then all global layers will be used in the query. If
+	 * objects is not specfied, then all object types will be used in the query. 
+	 *
+	 * 
+	 * @param lat the latitude
+	 * @param lon the longitude
+	 * @param radius the radius of the search in km
+	 * @param layers the layers to search in
+	 * @param types the different types to look for
+	 * @param limit the maximum amount of records to return (defaults to 100)
+	 * @param type the handler responsible for creating the return object
+	 * @return if {@link com.simplegeo.client.service.LocationService#futureTask} is false
+	 * then the return value will be the result of the response based on the handler used. Otherwise,
+	 * the return value will be a {@link java.util.concurrent.FutureTask}.
 	 * @throws ClientProtocolException
 	 * @throws IOException
+	 * @see <a href="http://help.simplegeo.com/faqs/api-documentation/endpoints"</a>
 	 */
 	public Object nearby(double lat, double lon, double radius, List<String> layers, List<String> types, int limit, Handler type) 
 						throws ClientProtocolException, IOException {
@@ -383,11 +475,16 @@ public class LocationService {
 	}
 	
 	/**
-	 * @param lat
-	 * @param lon
-	 * @return
+	 * Reverse geocodes a lat/lon pair.
+	 * 
+	 * @param lat the latitude
+	 * @param lon the longitude
+	 * @return if {@link com.simplegeo.client.service.LocationService#futureTask} is false
+	 * then the return value will be the result of the response based on the handler used. Otherwise,
+	 * the return value will be a {@link java.util.concurrent.FutureTask}.
 	 * @throws ClientProtocolException
 	 * @throws IOException
+	 * @see <a href="http://help.simplegeo.com/faqs/api-documentation/endpoints"</a>
 	 */
 	public Object reverseGeocode(double lat, double lon)
 			throws ClientProtocolException, IOException {
@@ -399,7 +496,7 @@ public class LocationService {
 	}
 	
 	/**
-	 * @return
+	 * @return the Http client used to execute all requests
 	 */
 	public OAuthHttpClient getHttpClient() {
 		return this.httpClient;
@@ -601,8 +698,7 @@ public class LocationService {
 				
 				logger.debug( String.format("unable to locate layer for the %s", object));
 			} 			
-
-			
+	
 		}
 		
 		return recordId;
