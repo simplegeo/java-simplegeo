7fe8bd8af1052dc06001995ba3a3df6395802022
mefogle <mark@fogle.com>
2010-07-28 22:42:13 -0700

    Documentation.

--- src/com/simplegeo/client/urlconn/OAuthURLConnection.java
+++ src/com/simplegeo/client/urlconn/OAuthURLConnection.java
@@ -1,3 +1,32 @@
+/**
+ * Copyright (c) 2009-2010, SimpleGeo
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, 
+ * this list of conditions and the following disclaimer. Redistributions 
+ * in binary form must reproduce the above copyright notice, this list of
+ * conditions and the following disclaimer in the documentation and/or 
+ * other materials provided with the distribution.
+ * 
+ * Neither the name of the SimpleGeo nor the names of its contributors may
+ * be used to endorse or promote products derived from this software 
+ * without specific prior written permission.
+ *  
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS 
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE 
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, 
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package com.simplegeo.client.urlconn;
 
 import java.util.logging.Logger;
@@ -10,6 +39,13 @@ import oauth.signpost.exception.OAuthCommunicationException;
 import oauth.signpost.exception.OAuthExpectationFailedException;
 import oauth.signpost.exception.OAuthMessageSignerException;
 
+/**
+ * A wrapper around an OAuthConsumer (a DefaultOAuthConsumer in this case) designed to hide the
+ * complexities of request signing from the rest of the REST code. 
+ * 
+ * @author Mark Fogle
+ */
+
 public class OAuthURLConnection implements OAuthClientIfc {
 	
 	
@@ -17,11 +53,6 @@ public class OAuthURLConnection implements OAuthClientIfc {
 	
 	private OAuthConsumer token = null;
 	
-			
-	/**
-	 * @param connManager
-	 * @param params
-	 */
 	public OAuthURLConnection() {
 		super();
 	}
