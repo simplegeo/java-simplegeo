f14535151dbb7e623438e6cada21494c463ff4d2
Casey W Crites <crites.casey@gmail.com>
2011-10-17 13:14:15 -0700

    Added the @Test annotation that was omitted.

--- src/test/java/com/simplegeo/client/types/PointTest.java
+++ src/test/java/com/simplegeo/client/types/PointTest.java
@@ -1,9 +1,11 @@
 package com.simplegeo.client.types;
 
 import org.junit.Assert;
+import org.junit.Test;
 
 public class PointTest {
 
+	@Test
 	public void testNewPoint() {
 		Point point = new Point(37.0, -105.0);
 		Assert.assertEquals(37.0, point.getLat(), 0d);
